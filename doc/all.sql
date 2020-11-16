c0efd1b48d23e219f1310ebb2fef387d9540d6be
KingWyhgithub <h18182020494@163.com>
2020-11-17 01:46:20 +0800

    增加大章表chapter 并且生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -1,9 +1,32 @@
-drop table if exists `test`;
-create table `test` (
-  `id` char(8) not null default '' comment 'id',
-  `name` varchar(50) comment '名称',
-  primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='测试';
+-- 大章
+drop table if exists `chapter`;
+create table `chapter`
+(
+    `id`        char(8) not null comment 'id',
+    `course_id` char(8) comment '课程id',
+    `name`      varchar(50) comment '名称',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='大章';
 
-insert into `test` (id, name) values (1, '测试');
-insert into `test` (id, name) values (2, '测试2');
+insert into `chapter` (id, course_id, name)
+values ('00000001', '00000000', '测试大章01');
+insert into `chapter` (id, course_id, name)
+values ('00000002', '00000000', '测试大章02');
+
+
+
+drop table if exists `test`;
+create table `test`
+(
+    `id`   char(8) not null default '' comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='测试';
+
+insert into `test` (id, name)
+values (1, '测试');
+insert into `test` (id, name)
+values (2, '测试2');
+
