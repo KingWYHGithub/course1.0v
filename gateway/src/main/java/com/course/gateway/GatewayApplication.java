f63fd05b7cb2752ca6de40f7ae1a9610a841c843
KingWyhgithub <h18182020494@163.com>
2020-11-22 16:37:00 +0800

    解决了gateway跨域的配置

--- gateway/src/main/java/com/course/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -6,7 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -19,6 +24,26 @@ public class GatewayApplication {
 		Environment env=app.run(args).getEnvironment();
 		LOG.info("启动成功！！！");
 		LOG.info("gateway地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+	}
+
+	/**
+	 * 跨域配置
+	 * @return
+	 */
+	@Bean
+	public CorsWebFilter corsWebFilter(){
+	CorsConfiguration configuration=new CorsConfiguration();
+
+	configuration.setAllowCredentials(Boolean.TRUE);
+	configuration.addAllowedMethod("*");
+	configuration.addAllowedOrigin("*");
+	configuration.addAllowedHeader("*");
+	configuration.setMaxAge(3600L);
+
+	UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource(new PathPatternParser());
+	source.registerCorsConfiguration("/**",configuration);
+
+		return new CorsWebFilter(source);
 	}
 
 }
